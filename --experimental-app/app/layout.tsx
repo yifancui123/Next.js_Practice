b8720f6f6ec68a0cb0d1c7f8d43cc9f5e7af8ca8
Yifan Cui <yifan.cui@students.mq.edu.au>
2026-01-18 01:13:38 +1100

    Import QueryClient and QueryClientProvider in Provider.tsx

--- --experimental-app/app/layout.tsx
+++ --experimental-app/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Providers from "@/components/Provider"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
   );
